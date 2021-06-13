6ca610356004e3bbec8792bf7d5c0ba56c802b7b
Antonio Sarno <83610904+pizzax11@users.noreply.github.com>
2021-06-13 11:10:43 +0200

    Update HelloWorld.c

--- Hello World/HelloWorld.c
+++ Hello World/HelloWorld.c
@@ -1,12 +1,14 @@
+//OMP is a librery for Multi Processing
 #include <omp.h>
 #include <stdio.h>
 
 int main()
 {
     /* 
-    1) #pragma omp parallel: create a team of threads
-    1)omp_get_thread_num() return the ID of thread
-    2)omp_get_num_threads() return the number of thread of the team*/
+    - #pragma omp parallel: create a team of threads
+    - omp_get_thread_num() return the ID of thread
+    - omp_get_num_threads() return the number of thread of the team
+    */
     #pragma omp parallel
     printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
     return 0;
