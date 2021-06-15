1a636a6a7bbd524f1c1bac7421b396d535ebb5fc
Antonio Sarno <83610904+pizzax11@users.noreply.github.com>
2021-06-16 01:11:43 +0200

    Add files via upload

---
+++ Schedule/dynamic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <unistd.h>
+int main()
+{
+    int n_threads, i;
+
+    /*
+    Schedule allows you to create the scheme with which 
+    the threads distribute the work of an iteration of a cycle.
+    "dynamic": scheduling works on a "first come, first served" basis. In this way each thread 
+    has an iteration, when it ends it will be assigned the next iteration
+    */
+    #pragma omp parallel for private(i) schedule(dynamic) num_threads(4)
+    for(i=0; i<16; i++)
+    {
+        //wait i second
+        sleep(i);
+        printf("The thread %d has completed the iteration %d\n", omp_get_thread_num(), i);
+    }
+    printf("Tutti i thread hanno terminato!\n");
+    return 0;
+}
