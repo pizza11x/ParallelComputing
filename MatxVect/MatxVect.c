35a3150e6b9d8115d9da2f90d815797fff06eef3
Antonio Sarno <83610904+pizzax11@users.noreply.github.com>
2021-06-17 00:09:32 +0200

    Update MatxVect.c

--- MatxVect/MatxVect.c
+++ MatxVect/MatxVect.c
@@ -2,15 +2,71 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <time.h> 
 
 /*
 This is a function to calculate the product of a matrix by a vector.
 You have to call it in a main program.
 
-
 -n: rows
 -m: columns
 */
+
+double *matxvet(int n, int m, double *vect, double **matrix);
+
+int main()
+{
+    int n, m, i, j;
+    double **A;
+    double *b;
+    double *x;
+
+    printf("Inserire il numero di righe della matrice A: ");
+    scanf("%d", &n);
+    printf("Inserire il numero di colonne della matrice A: ");
+    scanf("%d", &m);
+    /*Allocation of matrix A with filling of random numbers from 1 to 100*/
+    A = (double **)calloc(n, sizeof(double *));
+    for (i=0; i < n; i++){
+        A[i] = (double *)calloc(m, sizeof(double));
+    }
+    srand(time(NULL)); 
+    for(i=0; i < n; i++){
+        for(j=0; j < m; j++){
+            A[i][j]= 1+rand()%100;
+        }
+    }
+    /*Allocation of vector b and x with filling the vector x of random numbers from 1 to 100*/
+    b = (double *)calloc(n, sizeof(double));
+    x = (double *)calloc(n, sizeof(double));
+    for(i=0; i < n; i++){
+            x[i] = 1+rand()%100;
+    }
+    /*Stamp matrix  A*/
+    printf("\nThe matrix A is:\n");
+    for(i=0; i < n; i++){
+        for(j=0; j < m; j++){
+            printf("[%f]\t", A[i][j]);
+        }
+        printf("\n");
+    }
+    /*Stamp vector x*/
+    printf("\nThe vector x is\n");
+    for(i=0; i < n; i++){
+            printf("[%f]\t", x[i]);
+    }
+    /*Calculate the solution vector with the function*/
+    b = matxvet(n,m,x,A);
+    /*stamp vector b*/
+    printf("\nThe vector b is:\n");
+    for(i=0; i < n; i++){
+            printf("[%f]\t", b[i]);
+    }
+    return 0;
+
+}
+
+
 
 double *matxvet(int n, int m, double *vect, double **matrix)
 {
@@ -28,7 +84,7 @@ double *matxvet(int n, int m, double *vect, double **matrix)
     for (i=0; i<n; i++)
     {
         for(j=0; j<m; j++)
-            sol[i] += matrix[i][j]*vect[j];
+            sol[i] += matrix[i][j]*vect[i];
     }
 
     return sol;
