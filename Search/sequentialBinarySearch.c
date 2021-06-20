769c193f24da85c4a57394e3f7efc2baafe3d701
Antonio Sarno <83610904+pizza11x@users.noreply.github.com>
2021-06-21 00:12:46 +0200

    Update sequentialBinarySearch.c

--- Search/sequentialBinarySearch.c
+++ Search/sequentialBinarySearch.c
@@ -19,6 +19,14 @@ int main()
     }
     printf("\nEnter the number to search: ");
     scanf("%d", &token);
+    /*
+    We pass the following values to the function:
+    - the far left index (0)
+    - the far right index (n-1)
+    - the number of threads
+    - the token to search for
+    - vector A
+    */
     result = binarySearch(0, n - 1, t, token, a);
 
     if (result < 0)
@@ -32,31 +40,56 @@ int main()
 int binarySearch(int left, int right, int t, int token, int *a)
 {
     int index = -1, i;
+    //Size, is the size of the sub-portion each thread must work on
     int size = (right - left + 1) / 2;
     omp_set_num_threads(t);
     omp_set_nested(1);
-#pragma omp parallel shared(a, token, left, size, index)
+    #pragma omp parallel shared(a, token, left, size, index)
     {
         int id = omp_get_thread_num();
+        /*
+        leftThread is the leftmost index on which each thread must work. 
+        It is calculated by adding to the index passed to the function the product between 
+        the id of the current thread and the size of its portion of the vector
+        */
         int leftThread = left + id * size;
+        /*
+        rightThread is the rightmost index on which each thread must work. 
+        it is calculated from the sum of its leftThread with its sub-portion size -1
+        */
         int rightThread = leftThread + size - 1;
         int middleThread;
-
+        
+        /*
+        the while loop must be executed until the portions of the vector are greater than 1 and therefore the extreme left 
+        is less than or equal to the extreme right
+        */
         while (leftThread <= rightThread)
         {
             middleThread = (rightThread + leftThread) / 2;
             if (a[middleThread] == token)
             {
-                
+                /*
+                If the element in the median position is equal to the token 
+                then the value is stored, and enables the while to exit
+                */
                 index = middleThread;
                 leftThread = rightThread + 1;
             }
             else if (token < a[middleThread])
             {
+                /*
+                Otherwise if the token is less than the element in the median position, 
+                rightThread becomes median-1
+                */
                 rightThread = middleThread - 1;
             }
             else
             {
+                /*
+                Otherwise if the token is greater than the element in the median position, 
+                leftThread becomes median+1
+                */
                 leftThread = middleThread + 1;
             }
         }
