42728d319d85073871f671f3cbff233091cb5e20
Antonio Sarno <83610904+pizza11x@users.noreply.github.com>
2021-06-23 01:55:48 +0200

    Update oddEvenSort.c

--- Sort/oddEvenSort.c
+++ Sort/oddEvenSort.c
@@ -3,6 +3,11 @@
 #include <omp.h>
 #include <time.h>
 
+/* 
+This program allows you to sort an array of different sizes.
+This algorithm is optimized as it avoids contention by using two variables for exchanges.
+A thread enters the parallel section only if these two fields are reset
+*/
 void oddEvenSort(int *a, int size);
 
 int main(){
@@ -30,14 +35,18 @@ int main(){
 
 void oddEvenSort(int *a, int size)
 {
-    int sw0=0, sw1 = 0, start = 0, i = 0, t= 0, temp;
-
+    //sw0 and sw1 are variables to control exchanges
+    int sw0=0, sw1 = 0, i = 0, t= 0, temp;
+   
+   //It runs the loop only if the odd part is completely sorted
     while(!sw1)
     {
+       
         sw0=1;
         sw1=1;
         #pragma omp parallel shared(a, size, sw0, sw1, t) private(temp, i)
         {
+           //Even part
             #pragma omp for
             for(i=0; i<size-1; i+=2)
             {
@@ -49,7 +58,12 @@ void oddEvenSort(int *a, int size)
                     sw0=0;
                 }
             }
-            if(!sw0 || !t){
+           /*
+           If you have traded on the even side, then you do the odd side as well
+           and if this is the first time the loop has run
+           */
+            if(!sw0 || !t)
+            {
                 #pragma omp for
                 for(i=1; i<size -1; i+=2)
                 {
@@ -61,8 +75,9 @@ void oddEvenSort(int *a, int size)
                         sw1=0;
                     }
                 }
+               //Sets that the cycle has run at least once.
+               t = 1;
             }
-            t = 1;
         }
     }
 }
